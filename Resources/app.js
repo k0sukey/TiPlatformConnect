de72010891eb08cdaf6cd737e63b52dcb1d77133
Kosuke Isobe <naughtydog0721@gmail.com>
2012-03-05 23:04:58 +0900

    rewrite the app.js

--- Resources/app.js
+++ Resources/app.js
@@ -1,49 +1,234 @@
-(function() {
-  var RUN_TESTS = false;
-  
-  if (RUN_TESTS) {
-    Ti.include('tests/tests.js');
-  } else {
-    var accessTokenKey = Ti.App.Properties.getString('twitterAccessTokenKey'),
-        accessTokenSecret = Ti.App.Properties.getString('twitterAccessTokenSecret');
+(function(){
+	var window = Ti.UI.createWindow();
 
-    var Twitter = require('twitter').Twitter;
+	var twitter = require('twitter').Twitter({
+			consumerKey: 'INSERT KEY HERE',
+			consumerSecret: 'INSERT SECRET HERE',
+			accessTokenKey: Ti.App.Properties.getString('twitterAccessTokenKey'),
+			accessTokenSecret: Ti.App.Properties.getString('twitterAccessTokenSecret')
+		});
+
+	var tumblr = require('tumblr').tumblr({
+			consumerKey: 'INSERT KEY HERE',
+			consumerSecret: 'INSERT SECRET HERE',
+			accessTokenKey: Ti.App.Properties.getString('tumblrAccessTokenKey'),
+			accessTokenSecret: Ti.App.Properties.getString('tumblrAccessTokenSecret')
+		});
+
+	var flickr = require('flickr').flickr({
+			consumerKey: 'INSERT KEY HERE',
+			consumerSecret: 'INSERT SECRET HERE',
+			accessTokenKey: Ti.App.Properties.getString('flickrAccessTokenKey'),
+			accessTokenSecret: Ti.App.Properties.getString('flickrAccessTokenSecret'),
+			callbackUrl: 'http://www.example.com/flickrcallback'
+		});
+
+	var foursquare = require('foursquare').foursquare({
+			consumerKey: 'INSERT KEY HERE',
+			consumerSecret: 'INSERT SECRET HERE',
+			accessTokenKey: Ti.App.Properties.getString('foursquareAccessTokenKey'),
+			callbackUrl: 'http://www.example.com/foursquarecallback'
+		});
+
+	var tableView = Ti.UI.createTableView({
+		data: [],
+		style: Ti.UI.iPhone.TableViewStyle.GROUPED
+	});
+	window.add(tableView);
+
+	var rows = [];
+
+	var twitterRow = Ti.UI.createTableViewRow({
+		touchEnabled: false,
+		selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+	});
+	rows.add(twitterRow);
+
+	var twitterLabel = Ti.UI.createLabel({
+		left: 10,
+		text: 'Sing in width Twitter'
+	});
+	twitterRow.add(twitterLabel);
+
+	var twitterAuthorize = function(event){
+		twitter.addEventListener('login', function(e){
+			Ti.App.Properties.setString('twitterAccessTokenKey', e.accessTokenKey);
+			Ti.App.Properties.setString('twitterAccessTokenSecret', e.accessTokenSecret);
+
+			if (e.success) {
+				twitter.request('1/account/verify_credentials.json', {}, 'GET', function(e){
+					if (e.success) {
+						var json = JSON.parse(e.result.text);
+
+						twitterLabel.setText(json.screen_name);
+						twitter.touchEnabled = false;
+						twitter.selectionStyle = Ti.UI.iPhone.TableViewCellSelectionStyle.NONE;
+
+						if (!event) {
+							twitterRow.removeEventListener('click', twitterAuthorize);
+						}
+					} else {
+						// error proc...
+					}
+				});
+			} else {
+				// error proc…
+			}
+		});
+
+		twitter.authorize();
+	};
+
+	if (twitter.authorized) {
+		twitterAuthorize();
+	} else {
+		twitterRow.touchEnabled = true;
+		twitterRow.selectionStyle = Ti.UI.iPhone.TableViewCellSelectionStyle.BLUE;
+		twitterCallback = true;
+
+		twitterRow.addEventListener('click', twitterAuthorize);
+	}
+
+	var tumblrRow = Ti.UI.createTableViewRow({
+		touchEnabled: false,
+		selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+	});
+	rows.add(tumblrRow);
+
+	var tumblrLabel = Ti.UI.createLabel({
+		left: 10,
+		text: 'Sing in width Tumblr'
+	});
+	tumblrRow.add(tumblrLabel);
+
+	var tumblrAuthorize = function(event){
+		tumblr.addEventListener('login', function(e){
+			Ti.App.Properties.setString('tumblrAccessTokenKey', e.accessTokenKey);
+			Ti.App.Properties.setString('tumblrAccessTokenSecret', e.accessTokenSecret);
+
+			if (e.success) {
+				tumblr.request('v2/user/info', {}, 'POST', function(e){
+					if (e.success) {
+						var json = JSON.parse(e.result.text);
+
+						tumblrLabel.setText(json.response.user.name);
+						tumblr.touchEnabled = false;
+						tumblr.selectionStyle = Ti.UI.iPhone.TableViewCellSelectionStyle.NONE;
+
+						if (!event) {
+							tumblrRow.removeEventListener('click', tumblrAuthorize);
+						}
+					} else {
+						// error proc...
+					}
+				});
+			} else {
+				// error proc…
+			}
+		});
+
+		tumblr.authorize();
+	};
+
+	if (tumblr.authorized) {
+		tumblrAuthorize();
+	} else {
+		tumblrRow.touchEnabled = true;
+		tumblrRow.selectionStyle = Ti.UI.iPhone.TableViewCellSelectionStyle.BLUE;
+		tumblrCallback = true;
+
+		tumblrRow.addEventListener('click', tumblrAuthorize);
+	}
+
+	var flickrRow = Ti.UI.createTableViewRow({
+		touchEnabled: false,
+		selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+	});
+	rows.add(flickrRow);
+
+	var flickrLabel = Ti.UI.createLabel({
+		left: 10,
+		text: 'Sing in width Flickr'
+	});
+	flickrRow.add(flickrLabel);
+
+	var flickrAuthorize = function(event){
+		flickr.addEventListener('login', function(e){
+			Ti.App.Properties.setString('flickrAccessTokenKey', e.accessTokenKey);
+			Ti.App.Properties.setString('flickrAccessTokenSecret', e.accessTokenSecret);
+
+			flickrLabel.setText(flickr.getUsername());
+			flickr.touchEnabled = false;
+			flickr.selectionStyle = Ti.UI.iPhone.TableViewCellSelectionStyle.NONE;
+
+			if (!event) {
+				flickrRow.removeEventListener('click', flickrAuthorize);
+			}
+		});
+
+		flickr.authorize();
+	};
+
+	if (flickr.authorized) {
+		flickrAuthorize();
+	} else {
+		flickrRow.touchEnabled = true;
+		flickrRow.selectionStyle = Ti.UI.iPhone.TableViewCellSelectionStyle.BLUE;
+		flickrCallback = true;
+
+		flickrRow.addEventListener('click', flickrAuthorize);
+	}
+
+	var foursquareRow = Ti.UI.createTableViewRow({
+		touchEnabled: false,
+		selectionStyle: Ti.UI.iPhone.TableViewCellSelectionStyle.NONE
+	});
+	rows.add(foursquareRow);
+
+	var foursquareLabel = Ti.UI.createLabel({
+		left: 10,
+		text: 'Sing in width Foursquare'
+	});
+	foursquareRow.add(foursquareLabel);
+
+	var foursquareAuthorize = function(event){
+		foursquare.addEventListener('login', function(e){
+			Ti.App.Properties.setString('foursquareAccessTokenKey', e.accessTokenKey);
+
+			if (e.success) {
+				foursquare.request('v2/users/self', {}, 'GET', function(e){
+					if (e.success) {
+						var json = JSON.parse(e.result.text);
+
+						foursquareLabel.setText(json.response.user.firstName);
+						foursquare.touchEnabled = false;
+						foursquare.selectionStyle = Ti.UI.iPhone.TableViewCellSelectionStyle.NONE;
+
+						if (!event) {
+							foursquareRow.removeEventListener('click', foursquareAuthorize);
+						}
+					} else {
+						// error proc...
+					}
+				});
+			} else {
+				// error proc…
+			}
+		});
+
+		foursquare.authorize();
+	};
+
+	if (foursquare.authorized) {
+		foursquareAuthorize();
+	} else {
+		foursquareRow.touchEnabled = true;
+		foursquareRow.selectionStyle = Ti.UI.iPhone.TableViewCellSelectionStyle.BLUE;
+		foursquareCallback = true;
+
+		foursquareRow.addEventListener('click', foursquareAuthorize);
+	}
     
-    var client = Twitter({
-      consumerKey: "INSERT KEY HERE",
-      consumerSecret: "INSERT SECRET HERE",
-      accessTokenKey: accessTokenKey, 
-      accessTokenSecret: accessTokenSecret
-    });
-    
-    var win = Ti.UI.createWindow({backgroundColor: 'white'}),
-        tableView = Ti.UI.createTableView();
-  
-    win.add(tableView);
-    win.open();
-    
-    client.addEventListener('login', function(e) {
-      if (e.success) {
-        Ti.App.Properties.setString('twitterAccessTokenKey', e.accessTokenKey);
-        Ti.App.Properties.setString('twitterAccessTokenSecret', e.accessTokenSecret);
-        
-        client.request("1/statuses/home_timeline.json", {count: 100}, 'GET', function(e) {
-          if (e.success) {
-            var json = JSON.parse(e.result.text), 
-                tweets = json.map(function(tweet) {
-                  return {title: tweet.text};
-                });
-            
-            tableView.setData(tweets);
-          } else  {
-            alert(e.error);
-          }
-        });
-      } else {
-        alert(e.error);
-      }
-    });
-    
-    client.authorize();
-  }
+	tableView.setData(rows);
+	window.open();
 })(this);
